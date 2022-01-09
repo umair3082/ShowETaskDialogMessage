47e993c494c19de6339829af954f580cf3437a28
Umair Rasheed <34655240+umair3082@users.noreply.github.com>
2022-01-09 14:54:25 +0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,2 @@
+commoncode cc = new commoncode();
+cc.showcustomermsg("Confirmation Required!", "Do You Want to Save Data?", eTaskDialogIcon.Bulb.ToString(), "", eTaskDialogBackgroundColor.Blue.ToString(), "Save Massage Confirmation");
